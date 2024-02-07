05b0abd5af58d655b842e14160911eab826bdab3
looha <looha@foxmail.com>
2024-02-07 23:17:30 +0800

    modify icon

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -7,33 +7,27 @@
       <span class="title">网站列表</span>
     </div>
     <!-- 网站列表 -->
-    <Swiper
-      v-if="siteLinks[0]"
-      :modules="[Pagination, Mousewheel]"
-      :slides-per-view="1"
-      :space-between="40"
-      :pagination="{
-        el: '.swiper-pagination',
-        clickable: true,
-        bulletElement: 'div',
-      }"
-      :mousewheel="true"
-    >
-      <SwiperSlide v-for="site in siteLinksList" :key="site">
+    <Swiper v-if="siteLinks[0]" :modules="[Pagination, Mousewheel]" :slides-per-view="1" :space-between="40" :pagination="{
+      el: '.swiper-pagination',
+      clickable: true,
+      bulletElement: 'div',
+    }" :mousewheel="true">
+      <SwiperSlide v-for="site in siteLinks" :key="site">
         <el-row class="link-all" :gutter="20">
           <el-col v-for="(item, index) in site" :span="8" :key="item">
-            <div
-              class="item cards"
-              :style="index < 3 ? 'margin-bottom: 20px' : null"
-              @click="jumpLink(item)"
-            >
-              <Icon size="26">
-                <component :is="siteIcon[item.icon]" />
-              </Icon>
-              <span class="name text-hidden">{{ item.name }}</span>
+            <div>
+              <div class="item cards" v-for="(itemTwice, indexTwice) in item" :key="itemTwice"
+                :style="index < 3 ? 'margin-bottom: 20px' : null" @click="jumpLink(itemTwice)">
+                <Icon size="26">
+                  <component :is="siteIcon[itemTwice.icon]" />
+                </Icon>
+                <span class="name text-hidden">{{ itemTwice.name }}</span>
+              </div>
+
             </div>
           </el-col>
         </el-row>
+
       </SwiperSlide>
       <div class="swiper-pagination" />
     </Swiper>
@@ -43,7 +37,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, ImagesRegular, CloudDownloadAlt, CloudUploadAlt} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, ImagesRegular, CloudDownloadAlt, CloudUploadAlt, } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -53,11 +47,13 @@ const store = mainStore();
 
 // 计算网站链接
 const siteLinksList = computed(() => {
-  const result = [];
+  let result = [];
   for (let i = 0; i < siteLinks.length; i += 6) {
     const subArr = siteLinks.slice(i, i + 6);
     result.push(subArr);
   }
+
+
   return result;
 });
 
@@ -98,37 +94,45 @@ onMounted(() => {
     display: flex;
     align-items: center;
     animation: fade 0.5s;
+
     .title {
       margin-left: 8px;
       font-size: 1.15rem;
       text-shadow: 0 0 5px #00000050;
     }
   }
+
   .swiper {
     left: -10px;
     width: calc(100% + 20px);
     padding: 5px 10px 0;
     z-index: 0;
+
     .swiper-slide {
       height: 100%;
     }
+
     .swiper-pagination {
       position: static;
       margin-top: 4px;
+
       :deep(.swiper-pagination-bullet) {
         background-color: #fff;
         width: 18px;
         height: 4px;
         border-radius: 4px;
         transition: opacity 0.3s;
+
         &:hover {
           opacity: 1;
         }
       }
     }
   }
+
   .link-all {
     height: 220px;
+
     .item {
       height: 100px;
       width: 100%;
@@ -153,16 +157,20 @@ onMounted(() => {
         font-size: 1.1rem;
         margin-left: 8px;
       }
+
       @media (min-width: 720px) and (max-width: 820px) {
         .name {
           display: none;
         }
       }
+
       @media (max-width: 720px) {
         height: 80px;
       }
+
       @media (max-width: 460px) {
         flex-direction: column;
+
         .name {
           font-size: 1rem;
           margin-left: 0;
@@ -170,6 +178,7 @@ onMounted(() => {
         }
       }
     }
+
     @media (max-width: 720px) {
       height: 180px;
     }
